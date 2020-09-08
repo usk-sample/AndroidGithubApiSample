f9654eb82d2c9ab157691fd4c2daf5c372a0cfb3
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2020-09-08 11:33:37 +0900

    feat: テキスト変更を監視

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,18 +2,53 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
 
-    private var viewModel: MyViewModel = MyViewModel(this.application)
+    private lateinit var viewModel: MyViewModel
     private lateinit var binding: ActivityMainBinding
+
+    private var timer: Timer = Timer()
+    private val delay: Long = 500
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel = MyViewModel(application)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.editText.addTextChangedListener(this)
+
+    }
+
+    private fun doSearch(text: String) {
+        
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        //NOP
+
+        val text = binding.editText.text.toString()
+
+        timer.cancel()
+        timer = Timer()
+        timer.schedule(delay) {
+            this@MainActivity.doSearch(text)
+        }
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        //NOP
     }
 }
